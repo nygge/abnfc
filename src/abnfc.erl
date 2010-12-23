nygge/abnfc

commit 353d46c1f6e9535bc82f7fe4a24bafd7f10a3ffa

Add -verbose option.

src/abnfc.erl
@@ -42,10 +42,13 @@ file(File, Opts) when is_list(Opts) ->
 	    case parse(Text, POpts) of
 		{ok, AST, _Rest} ->
 		    AST1 = abnfc_ast:ast_to_int_form(AST),
-		    io:format("~p~n",[AST1]),
+		    case proplists:get_bool(verbose,Opts) of
+			true -> io:format("~p~n",[AST1]);
+			false -> ok
+		    end,
 		    {ok, Code} = abnfc_gen:generate(AST1, GenOpts),
 		    {ok, GenFile} = write_file(Code, GenOpts),
-		    compile_file(GenFile, COpts, proplists:get_bool(noobj,Opts));
+		    compile_file(GenFile, COpts, Opts);
 		Error ->
 		    Error
 	    end;
@@ -146,7 +149,8 @@ gen_opts(Name, Opts) ->
 	       true -> binary;
 	       false -> list
 	   end,
-    [{mod,Mod},Type].
+    Verbose = proplists:get_bool(verbose,Opts),
+    [{mod,Mod},{verbose,Verbose},Type].
 
 compiler_opts(Opts) ->
     OutDir = proplists:get_value(o, Opts, "./"),
@@ -156,13 +160,21 @@ compiler_opts(Opts) ->
 write_file(Code, Opts) ->
     Name = filename:join(proplists:get_value(o, Opts, "."),
 			 proplists:get_value(mod, Opts))++".erl",
-    io:format("abnfc: writing to ~p~n",[Name]),
+    
+    maybe_write("abnfc: writing to ~p~n",[Name],Opts),
     file:write_file(Name, Code),
     erl_tidy:file(Name,[{paper, 95},{backups,false}]),
     {ok,Name}.
 
-compile_file(File, Opts, false=_Noobj) ->
-    io:format("abnfc: compiling ~p opts = ~p~n",[File, Opts]),
-    compile:file(File, Opts);
-compile_file(_File, _Opts, true=_Noobj) ->
-    ok.
+compile_file(File, COpts, MyOpts) ->
+    maybe_write("abnfc: compiling ~p opts = ~p~n",[File, COpts],MyOpts),
+    case proplists:get_bool(noobj,MyOpts) of
+	true -> ok;
+	false -> compile:file(File, COpts)
+    end.
+
+maybe_write(Fmt,Args,Opts) ->
+    case proplists:get_bool(verbose,Opts) of
+	true -> io:format(Fmt,Args);
+	false -> ok
+    end.

bin/abnfc
@@ -22,7 +22,7 @@ do_it(File, Opts) ->
     io:format("~p~n",[R]).
     
 usage() ->
-    io:format("~s~n",["Usage: abnfc [-binary] [-noobj] [-o OutDir] [-I InclDir] [-mod Module] File"]).
+    io:format("~s~n",["Usage: abnfc [-binary] [-noobj] [-verbose] [-o OutDir] [-I InclDir] [-mod Module] File"]).
 
 parse_args(Args) ->
     parse_args(Args, []).
@@ -35,6 +35,8 @@ parse_args(["-binary"|More], Acc) ->
     parse_args(More, [binary|Acc]);
 parse_args(["-noobj"|More], Acc) ->
     parse_args(More, [noobj|Acc]);
+parse_args(["-verbose"|More], Acc) ->
+    parse_args(More, [verbose|Acc]);
 parse_args(["-I",IncludeDir|More], Acc) ->
     parse_args(More, [{i,IncludeDir}|Acc]);
 parse_args([File], Acc) ->

src/abnfc_gen.erl
@@ -51,7 +51,8 @@
 %% @end
 %%--------------------------------------------------------------------
 generate(AST, Opts) ->
-    io:format("Opts ~p~n",[Opts]),
+    Verbose = proplists:get_bool(verbose,Opts),
+    maybe_write("Opts ~p~n",[Opts],Verbose),
     Module = proplists:get_value(mod,Opts),
     Type = case proplists:get_bool(binary, Opts) of
 	       true -> binary;
@@ -63,7 +64,7 @@ generate(AST, Opts) ->
 		   exports(Names),
 		   attribute(atom(include),[string(lists:concat([Module,".hrl"]))]),
 		   gen_dec(Names),
-		   form_list([form_list([gen_rule(R, Type)]) || R <- AST]),
+		   form_list([form_list([gen_rule(R, Type, Verbose)]) || R <- AST]),
 		   mk__alt(),
 		   mk__repeat(),
 		   mk__seq(),
@@ -92,13 +93,13 @@ gen_dec(Names) ->
 	     [clause([atom(Name),variable('Str')],[],
 		     [application(application(atom(Name),[]),[variable('Str')])])||Name<-Names]).
 
-gen_rule(#rule{name=Name, body=Element, code=nocode}, Type) ->
-    io:format("abnfc_gen: generating rule ~p~n",[Name]),
+gen_rule(#rule{name=Name, body=Element, code=nocode}, Type, Verbose) ->
+    maybe_write("abnfc_gen: generating rule ~p~n",[Name],Verbose),
     Body = gen_elem(Element, Type),
     mk_rule_fun(Name, Body);
 
-gen_rule(#rule{name=Name, body=Element, code=Code}, Type) ->
-    io:format("abnfc_gen: generating rule ~p~n",[Name]),
+gen_rule(#rule{name=Name, body=Element, code=Code}, Type, Verbose) ->
+    maybe_write("abnfc_gen: generating rule ~p~n",[Name],Verbose),
     Vars = gen_vars(Element),
     Body = gen_elem(Element, Type),
     mk_rule_fun(Name, Body, Vars, Code).
@@ -391,3 +392,7 @@ mk__seq() ->
 				[tuple([atom(ok),list([]),variable('T')])])])
 		     ])]).
 
+maybe_write(Fmt,Args,true) ->
+    io:format(Fmt,Args);
+maybe_write(_Fmt,_Args,false) ->
+    ok.

commit 8f7b9bf3ffa4519cf95f166d02477e32a1b9ce0b

Fix noobj option again
The option was broken again in 353d46c1f6e9535bc82f7fe4a24bafd7f10a3ffa.

src/abnfc.erl
@@ -48,7 +48,7 @@ file(File, Opts) when is_list(Opts) ->
 		    end,
 		    {ok, Code} = abnfc_gen:generate(AST1, GenOpts),
 		    {ok, GenFile} = write_file(Code, GenOpts),
-		    compile_file(GenFile, COpts, Opts);
+		    compile_file(GenFile, COpts, proplists:get_bool(noobj,Opts));
 		Error ->
 		    Error
 	    end;